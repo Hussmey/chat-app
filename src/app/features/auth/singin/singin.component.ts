0f178881d0b2fc143638b38d693c21e8fe19b201
Hussam ahtash <Hussamahtash11@gmail.com>
2023-05-13 00:33:51 +0200

    finish login form

--- src/app/features/auth/singin/singin.component.ts
+++ src/app/features/auth/singin/singin.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-singin',
@@ -6,8 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./singin.component.css']
 })
 export class SinginComponent {
-  onButtonClick() {
-    console.log('Button clicked!');
+
+  counter: number = 5;
+  show = false;
+  constructor(private router: Router) {}
+
+
+  showDiv() {
+    this.show = true;
+    setTimeout(() => {
+      this.router.navigate(['/singup']);
+    }, 5000);
+      // timer down from 5 to 1 
+      setInterval(() => {
+        if(this.counter > 0) {
+          this.counter--;
+        }
+      }, 1000);
   }
+  
 
 }
+
