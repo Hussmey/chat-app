04193e6d30b2bdebb8a534443351cc7d9bc66a56
Hussam ahtash <Hussamahtash11@gmail.com>
2023-05-15 21:01:42 +0200

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', pathMatch:'full', redirectTo: 'singin'},
   {
     path: 'singin',loadChildren:() => import('./features/auth/singin/singin.module')
     .then(m => m.SinginModule)
